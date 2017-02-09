ce8539c0b4d49b984f5f2391239f005c43af0974
Tony M <tmelo1@jhu.edu>
2017-02-09 01:10:03 -0500

    edits

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -25,7 +25,6 @@ int main(int argc, char* argv[]) {
     course[LENGTH-1] = '\0';
 
     // TO DO: eventually remove the line below; it's just for debugging purposes 
-    printf("Course string collected: %s\n", course);
 
 
     // TO DO: add your code here
@@ -70,8 +69,8 @@ int main(int argc, char* argv[]) {
     }
 
     /* Converts the department strings and course numbers into decimal representations. */
-    int department = 100*course[3] + 10*course[4] + course[5];
-    int courseNo = 100*course[7] + 10*course[8] + course[9];
+    int department = 100*(course[3] - '0') + 10*(course[4] - '0') + (course[5] - '0');
+    int courseNo = 100*(course[7] - '0') + 10*(course[8] - '0') + (course[9] - '0');
 
     /* Encodes the letter grades for the course as an integer from 0-7 */
     switch(course[10]) {
@@ -115,18 +114,21 @@ int main(int argc, char* argv[]) {
     }
 
     /* No need to do any checking on this one. */
-    leftDecimal = course[12];
+    leftDecimal = course[12] - '0';
 
     /* Either encodes the 0 or 0.5 as a 0 or 1, respectively. */
-    if (course[14] == 0) {
+    if (course[14] == '0') {
         rightDecimal = 0;
     } else {
         rightDecimal = 1;
     }
 
     /* Simple formatting. */
-    printf("Division: %d\n Department: %d\n Course: %d\n Grade: %d %d\n Credits: %d %d",
-        division, department, courseNo, letterGrade, sign, leftDecimal, rightDecimal);
+    printf("Division: %d\n", division);
+    printf("Department: %d\n", department);
+    printf("Course: %d\n", courseNo); 
+    printf("Grade: %d %d\n", letterGrade, sign);
+    printf("Credits: %d %d\n", leftDecimal, rightDecimal);
 
   return 0;
 }
